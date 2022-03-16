1925dd4abdf8cab51af68f281d06fe11634832fb
eli-ezer <fuuzze@gmail.com>
2022-03-16 14:23:38 -0600

    metodos con mongoose

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,34 @@
 const express = require('express')
+const { append } = require('express/lib/response');
+const res = require('express/lib/response');
 const router = express.Router()
 
-router.get('/list',(req, res)=>{
-    res.send("lista de usuarios")
+var usuarios = [
+    {"id" : 1, "nombre" : "Corabella Halste", "Puesto": "Administrador"  },
+    {"id" : 2, "nombre" : "Francesco Huot", "Puesto": "Recursos Humanos"  },
+    {"id" : 3, "nombre" : "Ibrahim Downe", "Puesto": "Servicios"  },
+    {"id" : 4, "nombre" : "Milicent Dohrmann", "Puesto": "Ingeniero"  },
+    {"id" : 5, "nombre" : "Zandra Chrichton", "Puesto": "Soporte"  },
+    {"id" : 6, "nombre" : "Madelina Olenikov", "Puesto": "Juridico"  },
+    {"id" : 7, "nombre" : "Jone Scriver", "Puesto": "Vigilancia"  }
+    /* {"id" : 8, nombre : "Berna Zohrer", Puesto: "Vigilancia"  },
+    {"id" : 9, nombre : "Jacqui Mates", Puesto: "Administrador"  },
+    {"id" : 10, nombre : "Gaelan Cridland", Puesto: "Recursos Humanos"  },
+    {"id" : 11, nombre : "Hiram Lys", Puesto: "Servicios"  },
+    {"id" : 12, nombre : "Fleur Kellittz", Puesto: "Soporte"  },
+    {"id" : 13, nombre : "Lorenza Pryell", Puesto: "Marketing"  },
+    {"id" : 14, nombre : "Cherye Redley", Puesto: "Publicidad"  },
+    {"id" : 15, nombre : "Myca Bertrand", Puesto: "Ingeniero"  },
+    {"id" : 16, nombre : "Colver Jakel", Puesto: "Contabilidad"  },
+    {"id" : 17, nombre : "Hendrick Kennaird", Puesto: "Administrador"  },
+    {"id" : 18, nombre : "Kelbee Farrens", Puesto: "Gerencia"  },
+    {"id" : 19, nombre : "Sanford Tite", Puesto: "Gerencia"  },
+    {"id" : 20, nombre : "Burnard Brunskill", Puesto: "Vigilancia"  } */
+];
+
+router.get('/usuarios/',(req, res)=>{
+    res.send("lista de usuarios");
+    res.json(usuarios);
 })
 
 router.get('/id/:id',(req, res)=>{
--- server.js
+++ server.js
@@ -1,16 +1,74 @@
 const express = require('express');/*invocar el módulo de terceros - express*/
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/smart',{userNewUrlParser:true})
+    .then(()=> console.log('conectado a la BD smart'))
+    .catch(()=> console.log('error al conectarse a la BD'))
+
+const usuarioSchema = new mongoose.Schema({
+    nombre: String,
+    apellidos: String,
+    puesto: String
+})
+
+const Usuario = mongoose.model('usuario', usuarioSchema)
+
+getUsuarios()
+async function getUsuarios(){
+    const usuarios = await Usuario.find()
+    console.log(usuarios)
+}
+
+//createUsuario(); // para ejecutar la creacion cada que se corra el archivo
+async function createUsuario(){
+    const usuario = new Usuario({
+        nombre: String,
+        apellidos: String,
+        puesto: String
+    });
+
+    const result = await usuario.save();
+    console.log(result);
+}
 
 const { append } = require('express/lib/response');
 const res = require('express/lib/response');
 
 const servidor = express();/* creando un objeto de express */
-const user = require('./routes/user');
 servidor.use(express.json())
-servidor.use('/api/', user)
+/* const user = require('./routes/user');
+servidor.use('/api/', user) */
 const port = process.env.PORT || 3000
 
 servidor.listen(port, ()=> {
 
     console.log('Servidor en linea en el puerto: ' + port); /* estableciendo el puerto para TCP para nuestro servidor express */
+});
+
+var usuarios = [
+    {"id" : 1, "nombre" : "Corabella Halste", "puesto": "Administrador"  },
+    {"id" : 2, "nombre" : "Francesco Huot", "puesto": "Recursos Humanos"  }
+/*     {"id" : 3, nombre : "Ibrahim Downe", puesto: "Servicios"  },
+    {"id" : 4, nombre : "Milicent Dohrmann", puesto: "Ingeniero"  },
+    {"id" : 5, nombre : "Zandra Chrichton", puesto: "Soporte"  },
+    {"id" : 6, nombre : "Madelina Olenikov", puesto: "Juridico"  },
+    {"id" : 7, nombre : "Jone Scriver", puesto: "Vigilancia"  },
+    {"id" : 8, nombre : "Berna Zohrer", puesto: "Vigilancia"  },
+    {"id" : 9, nombre : "Jacqui Mates", puesto: "Administrador"  },
+    {"id" : 10, nombre : "Gaelan Cridland", puesto: "Recursos Humanos"  },
+    {"id" : 11, nombre : "Hiram Lys", puesto: "Servicios"  },
+    {"id" : 12, nombre : "Fleur Kellittz", puesto: "Soporte"  },
+    {"id" : 13, nombre : "Lorenza Pryell", puesto: "Marketing"  },
+    {"id" : 14, nombre : "Cherye Redley", puesto: "Publicidad"  },
+    {"id" : 15, nombre : "Myca Bertrand", puesto: "Ingeniero"  },
+    {"id" : 16, nombre : "Colver Jakel", puesto: "Contabilidad"  },
+    {"id" : 17, nombre : "Hendrick Kennaird", puesto: "Administrador"  },
+    {"id" : 18, nombre : "Kelbee Farrens", puesto: "Gerencia"  },
+    {"id" : 19, nombre : "Sanford Tite", puesto: "Gerencia"  },
+    {"id" : 20, nombre : "Burnard Brunskill", puesto: "Vigilancia"  } */
+];
+
+servidor.get('/api/usuarios/',(req, res)=>{
+    res.send("lista de usuarios");
+    res.json(usuarios);
 });
 
