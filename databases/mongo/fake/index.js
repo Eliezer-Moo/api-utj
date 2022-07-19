f5aaa34fe592d4036dd24936301e156e5131ebab
eli-ezer <fuuzze@gmail.com>
2022-07-19 12:26:42 -0500

    funcion para genera fake data para colecciones ingress y egress

--- databases/mongo/fake/index.js
+++ databases/mongo/fake/index.js
@@ -1,11 +1,20 @@
 const faker = require('faker');
+const {ingressModel, egressModel} = require('../');
 
-module.exports = () =>{
-    for (let i = 0; i < 10000; i++) {
-        const newIngress ={
+module.exports = async () =>{
+    for (let i = 0; i < 10; i++) {
+        const newIngress = new ingressModel(   {
             ingress: "ingress",
-            date: faker.date.betweens('2022-01-01T00:00:00.000Z', '2022-12-31T00:00:00.000Z')
-        }
+            date: faker.date.between('2022-12-01T00:00:00.000Z', '2022-01-01T00:00:00.000Z')
+        });
+        await newIngress.save();
+
+        const newEgress = new egressModel({
+            egress: 'egress',
+            date: faker.date.between('2022-12-01T00:00:00.000Z', '2022-01-01T00:00:00.000Z')
+        });
+
+        await newEgress.save();
         
     }
 };
