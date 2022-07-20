bad63af78506a8a2d1408476927b396656f1c495
Eliezer Moo <fuuzze@gmail.com>
2022-07-19 20:30:15 -0500

    formateando fecha

--- src/controllers/ingress.js
+++ src/controllers/ingress.js
@@ -1,8 +1,16 @@
+const momment = require('moment');
 const {mongo:{ingressModel}} = require('../../databases');
 
 module.exports ={
     getAll: async (req, res)=>{
-        const ingress = await ingressModel.find({},{_id:0, __v:0})
+        //const ingress = await ingressModel.find({},{_id:0, __v:0})
+        
+        const ingress = await ingressModel.find({},{_id:0, __v:0, device:0, ingress:0})
+        ingress.forEach(dateC =>{
+            let fecha = momment(dateC.date).format('L');
+            ingress.date = fecha;
+        });
+        
         res.json(ingress);
 },
     createOne: async (req, res)=>{
